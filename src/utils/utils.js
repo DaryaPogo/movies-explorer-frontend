192ae52737445f576f7d5bda6c006773834c020e
Darya Pogo <pogorelaja.darya@yandex.ru>
2023-07-09 01:17:46 +0300

    url add

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
 export const adress = "https://api.nomoreparties.co/beatfilm-movies",
-  baseURL = "http://localhost:3001",
+  // baseURL = "http://localhost:3000",
+  baseURL = 'https://api.diplom.dashapogo.nomoredomains.monster',
   movieAdress = "https://api.nomoreparties.co"
