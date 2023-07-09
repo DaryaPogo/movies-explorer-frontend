81c33c116824b160f75b45ab813c2b3d77c1a7f3
Darya Pogo <pogorelaja.darya@yandex.ru>
2023-07-09 10:12:38 +0300

    url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 export const adress = "https://api.nomoreparties.co/beatfilm-movies",
-  baseURL = "http://localhost:3000",
-  // baseURL = 'https://api.diplom.dashapogo.nomoredomains.monster',
+  // baseURL = "http://localhost:3000",
+  baseURL = 'https://api.diplom.dashapogo.nomoredomains.monster',
   movieAdress = "https://api.nomoreparties.co"
